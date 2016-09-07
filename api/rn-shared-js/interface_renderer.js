c30b12793153877c049923faa4218f6a5ca06918
Ric Wright <rkwright@geofx.com>
2016-09-07 13:34:52 -0500

    Minor tweaks to a couple of APIs

--- api/rn-shared-js/interface_renderer.js
+++ api/rn-shared-js/interface_renderer.js
@@ -1,6 +1,6 @@
 var interface_renderer =
 [
-    [ "addHighlight", "interface_renderer.html#a9a3a1b6310b82aff9c1a4b7ad78c2d5a", null ],
+    [ "addHighlight", "interface_renderer.html#a366ee20d739f5323ca0d770e1ae209d2", null ],
     [ "attachHandlerForHighlights", "interface_renderer.html#aa802b5d9e99b9ca4e7c2815e063e0e1a", null ],
     [ "clearHighlight", "interface_renderer.html#a6b5351742fe35fb895793645b6f7a3dc", null ],
     [ "clearHighlights", "interface_renderer.html#aeb09f3ce57d6b18a78e2faedd2a2100a", null ],
--- api/rn-shared-js/search/functions_61.js
+++ api/rn-shared-js/search/functions_61.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['addhandlerforevent',['addHandlerForEvent',['../interface_events.html#ad0f7277e6aaddb587ff543109491d9aa',1,'Events']]],
-  ['addhighlight',['addHighlight',['../interface_renderer.html#a9a3a1b6310b82aff9c1a4b7ad78c2d5a',1,'Renderer']]],
+  ['addhighlight',['addHighlight',['../interface_renderer.html#a366ee20d739f5323ca0d770e1ae209d2',1,'Renderer']]],
   ['attachhandlerforeventid',['attachHandlerForEventID',['../interface_events.html#a3e460537d027760ce29483b8f6f59c74',1,'Events']]],
   ['attachhandlerforhighlights',['attachHandlerForHighlights',['../interface_renderer.html#aa802b5d9e99b9ca4e7c2815e063e0e1a',1,'Renderer']]]
 ];
