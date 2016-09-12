b61dec28a4e7dc4c53bc8be367d2837389c19171
Ric Wright <rkwright@geofx.com>
2016-09-12 11:19:27 -0500

    Moved margins to PageLayout, other minor doc changes.

--- api/rn-shared-js/search/functions_73.js
+++ api/rn-shared-js/search/functions_73.js
@@ -9,8 +9,9 @@ var searchData=
   ['setgutterwidth',['setGutterWidth',['../interface_page_layout.html#aba90d0720c247664556aaace41104797',1,'PageLayout']]],
   ['sethighlightcolor',['setHighlightColor',['../interface_media_overlay.html#a0e3bcca595f41ed1c5998f172973d08f',1,'MediaOverlay']]],
   ['setjustification',['setJustification',['../interface_text_rendering.html#ac236bef553d6451fd2c119a8775c8e8c',1,'TextRendering']]],
+  ['setlayoutwidth',['setLayoutWidth',['../interface_page_layout.html#ab23e4c7bf0f109a5e4ac3d371c245da8',1,'PageLayout']]],
   ['setlineheight',['setLineHeight',['../interface_text_rendering.html#a378f42cab1210aecf314ea72fe2dbf55',1,'TextRendering']]],
-  ['setmargins',['setMargins',['../interface_text_rendering.html#a6d580d69dea1e3f0ac72eed3b9db5739',1,'TextRendering']]],
+  ['setmargins',['setMargins',['../interface_page_layout.html#a825925fe593953269457b86f70f1e32c',1,'PageLayout']]],
   ['setnumcolumns',['setNumColumns',['../interface_text_rendering.html#ab2a03cb21a7ac50768490869647e00cc',1,'TextRendering']]],
   ['setopacity',['setOpacity',['../interface_highlight_item.html#ab07dfa41f680c2c22cea6b98022299e6',1,'HighlightItem']]],
   ['setplaybackrate',['setPlaybackRate',['../interface_media_overlay.html#ac736fb43eff2cb8f1ae909e5822e09a6',1,'MediaOverlay']]],
@@ -18,7 +19,6 @@ var searchData=
   ['setrenditionorientation',['setRenditionOrientation',['../interface_page_layout.html#a3835413efc0f19f55d478c5c6f22ded3',1,'PageLayout']]],
   ['setrenditionspread',['setRenditionSpread',['../interface_page_layout.html#a65eccc262fed4372274528e6956f14c5',1,'PageLayout']]],
   ['settouchtoplay',['setTouchToPlay',['../interface_media_overlay.html#a95b77a98c1f24c8ca8e9be79e7d8b8b1',1,'MediaOverlay']]],
-  ['setwidth',['setWidth',['../interface_page_layout.html#aeb662399a3b8256d5a55f235827a6e7f',1,'PageLayout']]],
   ['setzoom',['setZoom',['../interface_transform.html#a1d265cb6422f8ff7c9f4b5736a74c619',1,'Transform']]],
   ['stop',['stop',['../interface_media_overlay.html#a5066b1b9896da9eb345c63080880234f',1,'MediaOverlay']]]
 ];
