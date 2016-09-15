30406618b10cc629fca5f62a3ddea49f11cb457e
Ric Wright <rkwright@geofx.com>
2016-09-15 10:56:29 -0500

    Changed Media Overlay highlightColor to moHighlightColor

--- api/rn-shared-js/interface_media_overlay.js
+++ api/rn-shared-js/interface_media_overlay.js
@@ -8,7 +8,7 @@ var interface_media_overlay =
     [ "getEnableAutomaticPageTurn", "interface_media_overlay.html#a30ebab3f78b91a099ec2851fbe304bfa", null ],
     [ "getEnableScrollDuringPlayback", "interface_media_overlay.html#aaf59fdbc69fa55016f6c159b7de7dac9", null ],
     [ "getEnableSkippability", "interface_media_overlay.html#af7bece36954b5cd5075d2b52e1e25655", null ],
-    [ "getHighlightColor", "interface_media_overlay.html#ae9c88c4fad8b2b070517ad875cf4aad4", null ],
+    [ "getMOHighlightColor", "interface_media_overlay.html#a68a47e943a9a340dc635b86087922ed9", null ],
     [ "getPlaybackRate", "interface_media_overlay.html#a2e6e7aebd154557ef36facb9f0085aab", null ],
     [ "getTouchToPlay", "interface_media_overlay.html#a498c52db160daabefa8f6f850d3d74ee", null ],
     [ "hasMediaOverlay", "interface_media_overlay.html#a26d69c63b07da67b2c2f971e429d6981", null ],
@@ -18,7 +18,7 @@ var interface_media_overlay =
     [ "moStop", "interface_media_overlay.html#a462ff6610b8bf59cca3b5e4f93b422ab", null ],
     [ "onMOError", "interface_media_overlay.html#ae126de1c0ef9fb9f4795159f53a7a38b", null ],
     [ "setAudioVolume", "interface_media_overlay.html#a5da55e210268cdc263ae17dfdd02de1c", null ],
-    [ "setHighlightColor", "interface_media_overlay.html#a0e3bcca595f41ed1c5998f172973d08f", null ],
+    [ "setMOHighlightColor", "interface_media_overlay.html#a9c6297e2c0526f872746f072efd0ae8f", null ],
     [ "setPlaybackRate", "interface_media_overlay.html#ac736fb43eff2cb8f1ae909e5822e09a6", null ],
     [ "setTouchToPlay", "interface_media_overlay.html#a95b77a98c1f24c8ca8e9be79e7d8b8b1", null ]
 ];
